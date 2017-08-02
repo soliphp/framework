fa745ac0e729f3e059950f1b7ac11d67f52293fe
ueaner <ueaner@gmail.com>
2017-08-03 03:31:37 +0800

    添加助手函数库 helpers.php

---
+++ tests/HelperTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Soli\Tests;
+
+class HelperTest extends TestCase
+{
+    public function testCamelize()
+    {
+        $this->assertEquals('CocoBongo', camelize('coco_bongo'));
+        $this->assertEquals('Co_coBon_go', camelize('co_co-bon_go', '-'));
+        $this->assertEquals('CoCoBonGo', camelize('co_co-bon_go', '_-'));
+    }
+
+    public function testUncamelize()
+    {
+        $this->assertEquals('coco_bongo', uncamelize('CocoBongo'));
+        $this->assertEquals('coco-bongo', uncamelize('CocoBongo', '-'));
+    }
+
+    public function testLower()
+    {
+        $this->assertEquals('hello', lower('HELLO'));
+    }
+
+    public function testUpper()
+    {
+        $this->assertEquals('HELLO', upper('hello'));
+    }
+
+    public function testStartsWith()
+    {
+        $this->assertTrue(starts_with('Hello', 'He'));
+        $this->assertFalse(starts_with('Hello', 'he'));
+    }
+
+    public function testEndsWith()
+    {
+        $this->assertTrue(ends_with('Hello', 'llo'));
+        $this->assertFalse(ends_with('Hello', 'LLO'));
+    }
+}
