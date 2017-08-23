7d7b305b8deb69901fa1f2afbd2863e18282227a
ueaner <ueaner@gmail.com>
2017-08-23 12:08:47 +0800

    :white_check_mark: 增加测试

--- tests/Data/Events/EComponent.php
+++ tests/Data/Events/EComponent.php
@@ -7,15 +7,4 @@ use Soli\Events\EventManagerAwareTrait;
 class EComponent
 {
     use EventManagerAwareTrait;
-
-    public function someTask()
-    {
-        $before = $this->eventManager->fire('my-component:before', $this);
-
-        // do something ...
-
-        $after = $this->eventManager->fire('my-component:after', $this);
-
-        return "$before, do something, $after";
-    }
 }
--- tests/Events/EventManagerTest.php
+++ tests/Events/EventManagerTest.php
@@ -11,6 +11,16 @@ use Soli\Tests\Data\Events\EComponentEvents;
 
 class EventManagerTest extends TestCase
 {
+    public function testTrait()
+    {
+        $eventManager = new EventManager;
+        $eComponent = new EComponent;
+
+        $eComponent->setEventManager($eventManager);
+
+        $this->assertTrue($eventManager === $eComponent->getEventManager());
+    }
+
     public function testFireByClosure()
     {
         $eventManager = new EventManager;
@@ -22,12 +32,8 @@ class EventManagerTest extends TestCase
             }
         );
 
-        $eComponent = new EComponent;
-        $eComponent->setEventManager($eventManager);
-
-        $result = $eComponent->someTask();
-
-        $this->assertStringStartsWith('before, do something', $result);
+        $result = $eventManager->fire('my-component:before', $eventManager);
+        $this->assertStringStartsWith('before', $result);
     }
 
     public function testFireByInstance()
@@ -39,11 +45,72 @@ class EventManagerTest extends TestCase
             new EComponentEvents()
         );
 
-        $eComponent = new EComponent();
-        $eComponent->setEventManager($eventManager);
+        $result = $eventManager->fire('my-component:before', $eventManager);
+        $this->assertNull($result);
 
-        $result = $eComponent->someTask();
+        $result = $eventManager->fire('my-component:after', $eventManager);
+        $this->assertStringStartsWith('after', $result);
+    }
 
-        $this->assertStringEndsWith('do something, after', $result);
+    public function testFireEmptyEvents()
+    {
+        $eventManager = new EventManager();
+
+        $result = $eventManager->fire('events:empty', $eventManager);
+
+        $this->assertNull($result);
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testFireInvalidEventType()
+    {
+        $eventManager = new EventManager();
+
+        $eventManager->on(
+            'my-component:before',
+            function (Event $event, $eComponent) {
+                return 'before';
+            }
+        );
+
+        $eventManager->fire('invalidEventType', $eventManager);
+    }
+
+    public function testHasListeners()
+    {
+        $eventManager = new EventManager();
+
+        $eventManager->on(
+            'my-component:before',
+            function (Event $event, $eComponent) {
+                return 'before';
+            }
+        );
+
+        $has = $eventManager->hasListeners('my-component:before');
+        $this->assertTrue($has);
+    }
+
+    public function testGetListeners()
+    {
+        $eventManager = new EventManager();
+
+        $eventManager->on(
+            'my-component:before',
+            function (Event $event, $eComponent) {
+                return 'before';
+            }
+        );
+
+        $listeners = $eventManager->getListeners('my-component:before');
+        $this->assertFalse(empty($listeners));
+
+        // off
+        $eventManager->off('my-component:before');
+
+        $listeners = $eventManager->getListeners('my-component:before');
+        $this->assertTrue(empty($listeners));
     }
 }
